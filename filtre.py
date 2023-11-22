8e374ecfdc7338b377c2a6b4baa4facd94010b0e
Gento91 <leo.kontente@gmail.com>
2023-11-22 09:42:38 +0100

    story 6

--- filtre.py
+++ filtre.py
@@ -14,7 +14,6 @@ def monochrome(imgname):
     imgchanged.save(f'./images-changed/{imgname}-flouté.png','png')
 
 def dilatation(imgname):
-    imgname='téléchargement.jpeg'
     img = cv2.imread(f'immages-test/{imgname}')
     kernel = np.ones((5,5), np.uint8)
     imgchanged=cv2.dilate(img, kernel, iterations=1)
@@ -25,7 +24,13 @@ def rotate(imgname,angle):
     imgchanged= img.rotate(angle)
     imgchanged.save(f'./images-changed/{imgname}-totate-{angle}°.png','png')
 
-def redimention(imgname):
-    img = Image.open (f'immages-test/{imgname}')
-    imgchanged = img.resize((256, 256))
-    imgchanged.save(f'./images-changed/{imgname}-flouté.png','png')
+def text(imgname,text):
+    img = cv2.imread(f'immages-test/{imgname}')
+    org = (50, 50) 
+    font = 3
+    fontScale = 1
+    color = (0,0,0) 
+    thickness = 2
+    imgchanged = cv2.putText(img, text, org, font,  
+                   fontScale, color, thickness, cv2.LINE_AA) 
+    cv2.imwrite(f'./images-changed/{imgname}-dilatée.png',imgchanged)
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from PIL import Image
-from PIL import ImageFilter
-import cv2
-import numpy as np
 
-angle=180
+from filtre import *
+
 imgname='téléchargement.jpeg'
-img=Image.open (f'immages-test/{imgname}')
-imgchanged= img.rotate(angle)
-imgchanged.save(f'./images-changed/{imgname}-totate-{angle}°.png','png')
+textim='test'
+
+image=text(imgname,textim)

75c5a631f862a093b97bb72885e3abce6054bf4e
Hearly <jonascompper2@gmail.com>
2023-11-22 10:23:20 +0100

    Story-7 terminé

--- filtre.py
+++ filtre.py
@@ -29,3 +29,33 @@ def redimention(imgname):
     img = Image.open (f'immages-test/{imgname}')
     imgchanged = img.resize((256, 256))
     imgchanged.save(f'./images-changed/{imgname}-flouté.png','png')
+
+def multiFiltre():
+    tabPic = []
+    askFiltre = input("Quel filtre veux-tu appliquer à tes images ? \n flou \n monochrome \n dilatation \n rotate \n redimension \n")
+    
+    if askFiltre in ["flou", "monochrome", "dilatation", "rotate", "redimension"]:
+        askPic = input("Donne moi le nom avec le format de l'image que tu veux cibler \n Si tu as fini de sélectionner toutes tes images, envoie rien \n ")
+        
+        while askPic != "":
+            tabPic.append(askPic)
+            askPic = input("Donne moi le nom avec le format de l'image que tu veux cibler \n Si tu as fini de sélectionner toutes tes images, envoie rien \n ")
+        
+        for i in range(len(tabPic)):
+            if askFiltre == "flou":
+                flou(tabPic[i])
+                
+            elif askFiltre == "monochrome":
+                monochrome(tabPic[i])
+                
+            elif askFiltre == "dilatation":
+                dilatation(tabPic[i])
+                
+            elif askFiltre == "rotate":
+                angle = int(input("Quel angle de rotation veux-tu pour les images ? "))
+                rotate(tabPic[i], angle)
+                
+            elif askFiltre == "redimension":
+                redimention(tabPic[i])
+
+multiFiltre()

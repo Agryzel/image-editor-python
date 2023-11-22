03d23446553ee69aeb74824800fd8a73acb1be5d
Hearly <jonascompper2@gmail.com>
2023-11-22 12:21:54 +0100

    story-12 terminer

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys
+from colorama import *
+import logger
 
-from filtre import *
+args = sys.argv[1:]
+if len(args) > 2:
+    print('Invalid number of arguments')
 
-imgname='téléchargement.jpeg'
-textim='test'
-
-image=text(imgname,textim)
+if len(args) == 1:
+    if args[0] == '-log':
+        logger.display_log()
+    else:
+        print("La commande que vous venez d'effectuer n'existe pas \n Utilisez la commande -help")
